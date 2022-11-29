a9b4a9d9da0305538a6108dfbc208fd4fcd43bfc
Alexander Antonov <al.antonov@corp.vk.com>
2022-11-29 21:36:20 +0300

    HW8 is completed

--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,57 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+	type CustomVars []struct {
+		name       string
+		value      string
+		needRemove bool
+	}
+
+	testParams := struct {
+		dir        string
+		customVars CustomVars
+	}{
+		dir: "testdata/test/",
+		customVars: CustomVars{
+			{name: "TEST_1", value: "TEST_VALUE_1", needRemove: false},
+			{name: "TEST_2", value: "", needRemove: true},
+		},
+	}
+
+	// init
+	_ = os.Mkdir(testParams.dir, 0o755)
+	for _, customVar := range testParams.customVars {
+		_ = os.WriteFile(testParams.dir+customVar.name, []byte(customVar.value), 0o755)
+	}
+
+	// cleanup
+	t.Cleanup(func() {
+		for _, customVar := range testParams.customVars {
+			_ = os.Remove(testParams.dir + customVar.name)
+		}
+		_ = os.Remove(testParams.dir)
+	})
+
+	// test
+	t.Run("Common test", func(t *testing.T) {
+		if env, err := ReadDir(testParams.dir); err == nil {
+			for _, customVar := range testParams.customVars {
+				value, ok := env[customVar.name]
+				require.True(t, ok)
+				require.Equal(t, value.Value, customVar.value)
+				require.Equal(t, value.NeedRemove, customVar.needRemove)
+			}
+
+			_, ok := env["UNDEFINED"]
+			require.False(t, ok)
+			require.Equal(t, len(testParams.customVars), len(env))
+		}
+	})
 }
--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,40 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+	for key, value := range env {
+		var err error
+		if !value.NeedRemove {
+			err = os.Setenv(key, value.Value)
+		} else {
+			err = os.Unsetenv(key)
+		}
+
+		if err != nil {
+			return 1
+		}
+	}
+
+	stdout, stderr := new(strings.Builder), new(strings.Builder)
+	executor := exec.Command(cmd[0], cmd[1:]...) //nolint:gosec
+	executor.Env = os.Environ()
+	executor.Stdout = stdout
+	executor.Stderr = stderr
+	if err := executor.Run(); err != nil {
+		_, _ = fmt.Fprint(os.Stderr, err)
+		return 1
+	}
+
+	if _, err := fmt.Fprint(os.Stdout, stdout.String()); err != nil {
+		return 1
+	}
+
+	return 0
 }
